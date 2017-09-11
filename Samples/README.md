1600285a01bd5951bbdeecd04a3ab21cab9641f1
梁桐铭 <werltm@hotmail.com>
2017-09-11 23:31:47 +0800

    now Initialize Project

---
+++ Samples/README.md
@@ -0,0 +1,4 @@
+# 摘要
+
+本演示项目是参考：https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro
+开发完成的。
---
+++ Samples/YoyocmsSchool/YoyocmsSchool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YoyocmsSchool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
